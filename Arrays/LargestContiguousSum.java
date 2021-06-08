878887c5d7bf58a7eaaccea505333943521b0e08
dpsingh <you@example.com>
2021-06-08 22:59:58 +0530

    added array questions

--- Arrays/LargestContiguousSum.java
+++ Arrays/LargestContiguousSum.java
@@ -5,5 +5,37 @@ package Arrays;
  */
 public class LargestContiguousSum {
       
+    int largestContiguousSum(int[] arr){
+	    
+        int psum =0, nsum =0, current =0;
+         
+        for(int i=0; i<arr.length; i++){
+          
+            current = arr[i];
+
+            if(current >=0){
+               
+               psum += current;
+
+            }else{
+           
+            if(nsum == 0){
+                
+            }
+
+
+
+            }
+
+        }   
+
+
+
+         return psum+nsum;
+
+	}
+    
     
 }
+
+
